d06546035a50f9c579f5b261f80b90eb23d0bc80
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-07-31 11:11:24 +0200

    Added vue router to the project

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
 
-Vue.config.productionTip = false
+import App from './App.vue';
+import router from './router';
+import VueRouter from 'vue-router';
 
+Vue.use(VueRouter);
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
