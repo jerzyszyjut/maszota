6ab227637092e5473367a58b57aa0ca140d03f0e
Zachariasz Jażdżewski <zachariasz.jazdzewski@outlook.com>
2021-08-02 15:42:35 +0200

    Created basic partners component layout

---
+++ src/components/Partners.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="partners">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Partners',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
