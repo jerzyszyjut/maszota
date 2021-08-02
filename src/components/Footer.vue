0ac26b6c97263dabfd047052129d548c1bc42403
Zachariasz Jażdżewski <zachariasz.jazdzewski@outlook.com>
2021-08-02 12:12:15 +0200

    Finished footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,18 @@
 <template>
     <div class="footer">
         <div class="upper">
-            <div class="upper-section" v-for="section in upperSections" :key="section"> 
-                <div class="text1"> {{ section.text1 }} </div>
-                <div class="text2"> {{ section.text2 }} </div>
+            <div class="section" v-for="section in upperSections" :key="section"> 
+                <div class="header"> 
+                    <div class="material-icons"> {{ section.icon }} &nbsp; </div>
+                    {{ section.header }}
+                </div>
+                <div class="content"> {{ section.content }} </div>
             </div>
         </div>
         <div class="lower">
-            <div class="lower-section" v-for="section in lowerSections" :key="section"> 
-                <div class="text1"> {{ section.text1 }} </div>
-                <span class="material-icons">
-                    local_phone
-                </span>
-                <div class="text2"> {{ section.text2 }} </div>
+            <div class="section low" v-for="section in lowerSections" :key="section"> 
+                <div class="header"> {{ section.header }} </div>
+                <div class="content"> {{ section.content }} </div>
             </div>
         </div>
     </div>
@@ -24,14 +24,14 @@ export default {
     data() {
         return {
             upperSections: [
-                {text1:'CALL US', text2:'Tel: 123-456-7890 | Fax: 123-456-7890'},
-                {text1:'EMAIL US', text2:'info@mysite.com'},
-                {text1:'OPENING HOURS', text2:'Mon - Fri: 7am - 10pm'},
+                {header:'CALL US', icon: 'local_phone', content:'Tel: 123-456-7890 | Fax: 123-456-7890'},
+                {header:'EMAIL US', icon: 'mail_outline', content:'info@mysite.com'},
+                {header:'OPENING HOURS', icon: 'schedule', content:'Mon - Fri: 7am - 10pm'},
             ],
             lowerSections: [
-                {text1:'LOREM IPSUM DOLOR SIT AMET', text2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-                {text1:'LOREM IPSUM DOLOR SIT AMET', text2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
-                {text1:'LOREM IPSUM DOLOR SIT AMET', text2:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
+                {header:'LOREM IPSUM DOLOR SIT AMET', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
+                {header:'LOREM IPSUM DOLOR SIT AMET', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
+                {header:'LOREM IPSUM DOLOR SIT AMET', content:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'},
             ]
         }
     }
@@ -42,38 +42,42 @@ export default {
     .footer {
         position: fixed;
         bottom: 0%;
+        box-sizing: border-box;
+        width: 100%;
+        background-color: #2F2E2E;
+        color: white;
+        padding-top: 30px;
+        padding-bottom: 50px;
     }
     .upper {
         display: flex;
         justify-content: center;
-        box-sizing: border-box;
-        width: 100%;
-        background-color: #2F2E2E;
     }
     .lower {
         display: flex;
         justify-content: center;
-        box-sizing: border-box;
-        width: 100%;
-        background-color: #2F2E2E;
     }
-    .upper-section {
+    .section {
         width: 15%;
-        padding: 30px 50px 0px 50px;
+        padding-left: 50px;
+        padding-right: 50px;
     }
-    .lower-section {
-        width: 15%;
-        padding: 25px 50px 50px 50px;
+    .section.low {
+        padding-top: 25px;
     }
-    .text1 {
-        color: white;
+    .section:nth-child(2) {
+        border-left: solid 2px;
+        border-right: solid 2px;
+    }
+    .header {
+        display: flex;
+        align-items: center;
         font-size: 20px;
     }
-    .lower .text1 {
+    .lower .header {
         margin-bottom: 15px;
     }
-    .text2 {
-        color: white;
+    .content {
         font-size: 14px;
     }
 </style>
