bdb647ac92ab5ca7b5cdfac92337dbddb5b7f42a
Zachariasz Jażdżewski <zachariasz.jazdzewski@outlook.com>
2021-08-02 16:17:06 +0200

    Finished Partners component

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="partners">
-        
+        <div class="header">LOREM IPSUM</div>
+        <div class="logos">
+            <img class="logo" src="../assets/partners_logos/bruh.png">
+            <img class="logo" src="../assets/partners_logos/bruh.png">
+            <img class="logo" src="../assets/partners_logos/bruh.png">
+        </div>
     </div>
 </template>
 
@@ -16,5 +21,37 @@ export default {
 </script>
 
 <style scoped>
-    
+    .partners {
+        box-sizing: border-box;
+        width: 100%;
+        background-color: white;
+        color: black;
+        padding: 100px;
+    }
+    .header {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 30px;
+        margin-bottom: 50px;
+    }
+    .logos {
+        display: flex;
+        justify-content: space-evenly;
+        align-items: center;
+    }
+    .logo {
+        height: 150px;
+    }
+    @media screen and (max-width: 600px) {
+        .partners {
+            padding: 10%;
+        }
+        .logos {
+            flex-direction: column;
+        }
+        .logo {
+            margin-bottom: 50px;
+        }
+    }
 </style>
