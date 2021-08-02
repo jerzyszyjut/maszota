4837c5a0d4809cce7eb8b79812ba5fc40d564848
Zachariasz Jażdżewski <zachariasz.jazdzewski@outlook.com>
2021-08-02 16:46:12 +0200

    Changed site font

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="home">
+    <NavBar/>
+    <Partners/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+import Partners from '../components/Partners.vue'
+import NavBar from '../components/NavBar.vue'
+import Footer from '../components/Footer.vue'
+
 export default {
   name: 'Home',
+  components: {
+    NavBar,
+    Partners,
+    Footer,
+  }
 }
 </script>
