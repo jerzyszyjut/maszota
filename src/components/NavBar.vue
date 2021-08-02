b36023f7f823024794f4e3b6c8af90a3db9ec1bb
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-02 10:31:25 +0200

    Added logo to navbar component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,23 @@
 <template>
     <div>
-        NavBar
+        <Logo :type="type"/>
     </div>
 </template>
 
 <script>
+import Logo from './Logo.vue';
+
 export default {
     name: 'Navbar',
+    props: {
+        type: {
+            default: 'navbar',
+            Number
+        }
+    },
+    components: {
+        Logo,
+    }
 }
 </script>
 
