f00c2ae840255156d68c420ccc00175e9faee8a9
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-02 11:34:49 +0200

    Created unresponsive navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,13 @@
 <template>
-    <div>
-        <Logo :type="type"/>
+    <div class="navbar">
+        <div class="navbar-content">
+            <Logo :type="type"/>
+            <div class="links">
+                <div class="link" v-for="link in links" :key="link.text">
+                    <router-link :to="link.address">{{ link.text }}</router-link>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -9,18 +16,54 @@ import Logo from './Logo.vue';
 
 export default {
     name: 'Navbar',
+    components: {
+        Logo,
+    },
     props: {
         type: {
-            default: 'navbar',
+            default: 'navbar-logo',
             Number
         }
     },
-    components: {
-        Logo,
-    }
+    data () {
+        return {
+            links: [
+                {text: 'HOME', address: '/#'},
+                {text: 'SERVICES', address: '/#'},
+                {text: 'ABOUT US', address: '/#'},
+                {text: 'CONTACT', address: '/#'},
+            ]
+        }
+    },
 }
 </script>
 
 <style scoped>
+.navbar {
+    background-color: rgb(253, 253, 253);
+}
+.navbar-content {
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
+    padding: 20px;
+}
+.links {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    padding-left: 50px;
+}
+.links .link {
+    padding: 0px 10px 0px 10px;
+}
+.links .link a{
+    text-decoration: none;
+    color: black;
+}
+.links .link a:hover{
+    color: rgb(235, 79, 171);
+}
 </style>
