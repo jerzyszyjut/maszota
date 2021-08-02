2d692ba7ef39938b2cf34818d7cfc66d77540da3
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-02 17:15:23 +0200

    Initialized i18n

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import router from './router/index';
 import VueRouter from 'vue-router';
 
 import './assets/style/global.css';
+import i18n from './i18n'
 
 Vue.use(VueRouter);
 new Vue({
   router,
-  render: h => h(App),
+  i18n,
+  render: h => h(App)
 }).$mount('#app');
 
