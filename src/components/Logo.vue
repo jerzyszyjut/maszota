2ac491e369e561f1400b07de7ad4ebc117a1b118
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-02 10:30:50 +0200

    Created logo component

---
+++ src/components/Logo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="logo" :class="type"></div>
+</template>
+
+<script>
+export default {
+    name: 'Logo',
+    props: {
+        type: {
+            required: true,
+            String,
+        },
+        width: {
+            Number,
+        },
+        height: {
+            Number,
+        },
+    },
+}
+</script>
+
+<style scoped>
+.logo {
+    background-image: url('../assets/img/logo.svg');
+    height: 100px;
+    width: 100px;
+    background-size: 100px 100px;
+}
+</style>
