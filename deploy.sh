12a702ceaf425fcf7c290a9fb666873393c981cb
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-03 19:53:09 +0200

    Prepared files for automatic project build

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+
+# build
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:jerzyszyjut/maszota.git master:gh-pages
+cd -
