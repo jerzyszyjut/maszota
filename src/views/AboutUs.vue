c015b4df5aa12b5727e17e510b19246bb9f8368f
Zachariasz Jażdżewski <zachariasz.jazdzewski@outlook.com>
2021-08-07 21:50:11 +0200

    Initial work on About-us

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,40 +1,38 @@
 <template>
     <div class="about-us">
+        <div class="stripe"></div>
         <div class="team">
-            <h2>{{ $t('aboutUs.team.header') }}</h2>
-            <p>{{ $t('aboutUs.team.firstParagraph') }}</p>
-            <p>{{ $t('aboutUs.team.secondParagraph') }}</p>
+            <div class="header">{{ $t('aboutUs.team.header') }}</div>
+            <div class="paragraph">{{ $t('aboutUs.team.firstParagraph') }}</div>
+            <div class="paragraph">{{ $t('aboutUs.team.secondParagraph') }}</div>
         </div>
+        <div class="header">{{ $t('aboutUs.history.header') }}</div>
         <div class="history">
-            <h2>{{ $t('aboutUs.history.header') }}</h2>
-            <v-timeline>
-                <v-timeline-item
-                    v-for="(year, i) in years"
-                    :key="i"
-                    small
-                >
+            <v-timeline reverse="reverse">
+                <v-timeline-item v-for="(year, i) in years" :key="i" :color="year.color" fill-dot>
                     <template v-slot:opposite>
-                    <span
-                        class="headline font-weight-bold"
+                        <span
+                        class="header"
+                        :style="`color: ${year.color};`"
                         v-text="year.year"
-                    ></span>
+                        ></span>
                     </template>
-                    <div class="py-4">
-                    <h2 class="headline font-weight-light mb-4">
-                        {{ $t(`aboutUs.history.years.${year.year}.header`) }}
-                    </h2>
-                    <div>
-                        {{ $t(`aboutUs.history.years.${year.year}.text`) }}
-                    </div>
+                    <div class="" style="max-width: 650px">
+                        <div :style="`color: ${year.color}; font-size: 22px; margin-bottom: 20px; text-transform: uppercase`">
+                            {{ $t(`aboutUs.history.years.${year.year}.header`) }}
+                        </div>
+                        <div>
+                            {{ $t(`aboutUs.history.years.${year.year}.text`) }}
+                        </div>
                     </div>
                 </v-timeline-item>
             </v-timeline>
         </div>
         <div class="company-basics">
-            <h2>{{ $t('aboutUs.companyBasics.header') }}</h2>
-            <p v-for="i in 5" :key="i">
+            <div class="header">{{ $t('aboutUs.companyBasics.header') }}</div>
+            <div class="bottom paragraph" v-for="i in 5" :key="i">
                 {{ $t(`aboutUs.companyBasics.paragraphs[${i-1}]`) }}
-            </p>
+            </div>
         </div>
     </div>
 </template>
@@ -47,27 +45,35 @@ export default {
             years: [
                 {
                     year: '2020',
+                    color: '#5B41A0',
                 },
                 {
                     year: '2018',
+                    color: '#693FA0',
                 },
                 {
                     year: '2016',
+                    color: '#783CA0',
                 },
                 {
                     year: '2014',
+                    color: '#9738A0',
                 },
                 {
                     year: '2010',
+                    color: '#A6359F',
                 },
                 {
                     year: '2008',
+                    color: '#B5329F',
                 },
                 {
                     year: '2006',
+                    color: '#C32E9E',
                 },
                 {
                     year: '2001',
+                    color: '#D22C9E',
                 },
             ]
         }
@@ -76,7 +82,52 @@ export default {
 </script>
 
 <style scoped>
-.v-timeline {
-    padding: 50px;
+.about-us {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.stripe {
+    background-image: linear-gradient(to right, #5B41A0, #E33EA7);
+    width: 100%;
+    height: 20px;
+}
+.header {
+    width: 60%;
+    font-size: 32px;
+    font-weight: 500;
+    text-align: center;
+    margin-bottom: 30px;
+    text-transform: uppercase;
+}
+.paragraph {
+    display: flex;
+    justify-content: center;
+    width: 30%;
+    font-size: 16px;
+    font-weight: 300;
+    text-align: center;
+    margin-bottom: 20px;
+}
+.bottom {
+    width: 40%;
+}
+.team {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 100px 0 100px 0;
+    background-color: #f3f3f3;
+    margin-bottom: 100px;
+}
+.company-basics {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    padding: 100px 0 100px 0;
+    background-color: #f3f3f3;
 }
 </style>
