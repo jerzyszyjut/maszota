e8ede5ff77735ddc594d78aaffeba5b124e101dd
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-07 11:58:34 +0200

    Implemented i18n on AboutUs page

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="about-us">
         <div class="team">
-            <h2>Profesjonalny i kreatywny zespół</h2>
-            <p>Wykorzystujemy nasz potencjał w drodze do doskonałości. Z zaangażowaniem dbamy o najwyższą jakość naszych usług</p>
-            <p>Nasza firma bazuje na efektywnej obsłudze klienta oraz najwyższym poziomie jakości napraw. Gwarancją jest nasza praca, którą wykonujemy już 20 lat</p>
+            <h2>{{ $t('aboutUs.team.header') }}</h2>
+            <p>{{ $t('aboutUs.team.firstParagraph') }}</p>
+            <p>{{ $t('aboutUs.team.secondParagraph') }}</p>
         </div>
         <div class="history">
-            <h2>Historia rozwoju zawodowego i doświadczenia</h2>
+            <h2>{{ $t('aboutUs.history.header') }}</h2>
             <v-timeline>
                 <v-timeline-item
                     v-for="(year, i) in years"
@@ -21,22 +21,20 @@
                     </template>
                     <div class="py-4">
                     <h2 class="headline font-weight-light mb-4">
-                        {{ year.header }}
+                        {{ $t(`aboutUs.history.years.${year.year}.header`) }}
                     </h2>
                     <div>
-                        {{ year.text }}
+                        {{ $t(`aboutUs.history.years.${year.year}.text`) }}
                     </div>
                     </div>
                 </v-timeline-item>
             </v-timeline>
         </div>
         <div class="company-basics">
-            <h2>Podstawy działania firmy</h2>
-            <p>Technologia, materiały i narzędzia odgrywają kluczową rolę w procesie naprawczym. Dlatego też szczególną uwagę skupiamy na ich doborze, aby uzyskać perfekcyjny efekt końcowy</p>
-            <p>Nasze życiowe i osobiste wartości kształtują format budowania naszej marki, autorytetu i prestiżu</p>
-            <p>Wiedza, doświadczenie i cieżka praca sprawiają, że jesteśmy stabilnym i niezawodnym partnerem oferującym usługi napraw blacharskich i lakierniczych z elementami detailingu na rynku motoryzacyjnym</p>
-            <p>Nasza lakiernia jest kompletna. Przemyślany został każdy jej element tak, aby w pełni wykorzystać potencjał ludzki i techniczny.</p>
-            <p>Dążymy do doskonałości. Rozwijamy się i kreujemy własny system działania. Zapewniamy najwyższą jakość i profesjonalizm. Nasze prace są wykonywane z precyzją i dbałością, a efekty są oryginalne i wyjątkowe</p>
+            <h2>{{ $t('aboutUs.companyBasics.header') }}</h2>
+            <p v-for="i in 5" :key="i">
+                {{ $t(`aboutUs.companyBasics.paragraphs[${i-1}]`) }}
+            </p>
         </div>
     </div>
 </template>
