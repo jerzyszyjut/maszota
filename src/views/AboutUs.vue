ace8cbf802c35a895062a402bd5ebf96e1406c08
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-08-07 11:09:58 +0200

    Copied text to about us site

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -1,6 +1,22 @@
 <template>
     <div class="about-us">
-        Hello world!
+        <div class="team">
+            <h2>Profesjonalny i kreatywny zespół</h2>
+            <p>Wykorzystujemy nasz potencjał w drodze do doskonałości. Z zaangażowaniem dbamy o najwyższą jakość naszych usług</p>
+            <p>Nasza firma bazuje na efektywnej obsłudze klienta oraz najwyższym poziomie jakości napraw. Gwarancją jest nasza praca, którą wykonujemy już 20 lat</p>
+        </div>
+        <div class="history">
+            <h2>Historia rozwoju zawodowego i doświadczenia</h2>
+            <p>Tu będzie timeline</p>
+        </div>
+        <div class="company-basics">
+            <h2>Podstawy działania firmy</h2>
+            <p>Technologia, materiały i narzędzia odgrywają kluczową rolę w procesie naprawczym. Dlatego też szczególną uwagę skupiamy na ich doborze, aby uzyskać perfekcyjny efekt końcowy</p>
+            <p>Nasze życiowe i osobiste wartości kształtują format budowania naszej marki, autorytetu i prestiżu</p>
+            <p>Wiedza, doświadczenie i cieżka praca sprawiają, że jesteśmy stabilnym i niezawodnym partnerem oferującym usługi napraw blacharskich i lakierniczych z elementami detailingu na rynku motoryzacyjnym</p>
+            <p>Nasza lakiernia jest kompletna. Przemyślany został każdy jej element tak, aby w pełni wykorzystać potencjał ludzki i techniczny.</p>
+            <p>Dążymy do doskonałości. Rozwijamy się i kreujemy własny system działania. Zapewniamy najwyższą jakość i profesjonalizm. Nasze prace są wykonywane z precyzją i dbałością, a efekty są oryginalne i wyjątkowe</p>
+        </div>
     </div>
 </template>
 
