a44dcd41015b6e06d5606fdb5eb9ca66858f712f
Jerzy Szyjut <kieszyk.pl@gmail.com>
2021-10-19 16:57:53 +0200

    Styled contact us page

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -2,7 +2,7 @@
     <div class="contact">
         <div class="contact-details">
             <div class="header">
-                <p>{{ $t('contact.header') }}</p>
+                <h1>{{ $t('contact.header') }}</h1>
             </div>
             <div class="content">
                 <div class="phone">
@@ -20,7 +20,7 @@
             </div>
         </div>
         <div class="map">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1701.8733531070754!2d17.75860756213939!3d54.596445950441876!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46fddd17538b8085%3A0x48a8603e00283f52!2sMASZOTA%20PROFESSIONAL%20CAR%20SPRAYING!5e1!3m2!1spl!2spl!4v1632572880540!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2311.3930566260597!2d17.758524!3d54.59706800000001!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x48a8603e00283f52!2sMASZOTA%20PROFESSIONAL%20CAR%20SPRAYING!5e0!3m2!1spl!2spl!4v1632591171461!5m2!1spl!2spl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>    
         </div>
     </div>
 </template>
@@ -30,3 +30,38 @@ export default {
     name: 'Contact',
 }
 </script>
+
+<style scoped>
+.contact {
+    margin: 100px 0px 0px 0px;
+}
+.header {
+    display: flex;
+    justify-content: center;
+    text-transform: uppercase;
+    text-align: center;
+}
+.content {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    padding: 50px;
+}
+.content div{
+    width: auto;
+    text-transform: uppercase;
+    margin: 10px 20px 10px 20px;
+}
+.map {
+    padding: 50px;
+    display: flex;
+    justify-content: center;
+    background-color: #ececec;
+}
+@media screen and (max-width: 600px) {
+    .content {
+        padding-top: 20px;
+        flex-direction: column;
+    }
+}
+</style>
